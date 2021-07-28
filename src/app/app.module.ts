f13f7f3d73aa504148feedf00872eb0e8baf4496
FaridaElsheikh <faridaelsheikh2000@gmail.com>
2021-07-28 13:58:20 +0300

    week 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,19 @@ import { MatButtonModule } from '@angular/material/button'
 
 import 'hammerjs';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { LeaderService } from './services/leader.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -25,7 +30,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -39,7 +47,9 @@ import { FooterComponent } from './footer/footer.component';
     MatButtonModule
   ],
   providers: [
-    DishService
+    DishService,
+    LeaderService,
+    PromotionService
   ],
   bootstrap: [AppComponent]
 })
