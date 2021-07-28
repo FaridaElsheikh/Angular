1c20bb6dd56f2f3c78a49d6c27a0d4e2995c3a84
FaridaElsheikh <faridaelsheikh2000@gmail.com>
2021-07-28 18:27:25 +0300

    reactive forms p1

---
+++ src/app/shared/feedback.ts
@@ -0,0 +1,10 @@
+export class Feedback{
+    firstname:string |undefined;
+    lastname:string |undefined;
+    email:string |undefined;
+    agree:boolean |undefined;
+    contacttype:string |undefined;
+    message:string |undefined;
+}
+
+export const ContactType = ['None','Tel','Email'];
